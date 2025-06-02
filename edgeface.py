7c5ac6a4d4eda0a2048a7f07065a13555d01a03f
nucleuserumagborie <nucleuserumagborie@gmail.com>
2025-06-02 09:16:33 +0000

    new models

--- edgeface.py
+++ edgeface.py
@@ -1,27 +1,12 @@
 import ai_edge_torch
-import numpy
 import torch
+import tensorflow as tf
 
 torch_model = torch.hub.load(
     "otroshi/edgeface", "edgeface_xs_gamma_06", source="github", pretrained=True
 )
 
-torch_model.eval()
-
 torch_inputs = (torch.randn(1, 3, 112, 112),)
-torch_output = torch_model(*torch_inputs)
-
-edge_model = ai_edge_torch.convert(torch_model, torch_inputs)
-edge_output = edge_model(*torch_inputs)
-
-if numpy.allclose(
-    torch_output.detach().numpy(),
-    edge_output,
-    atol=1e-5,
-    rtol=1e-5,
-):
-    print("Inference result with Pytorch and TfLite was within tolerance")
-else:
-    print("Something wrong with Pytorch --> TfLite")
-
-edge_model.export("./face-recognition.tflite")
+tfl_converter_flags = {'optimizations': [tf.lite.Optimize.DEFAULT]}
+edge_model = ai_edge_torch.convert(torch_model.eval(), torch_inputs, _ai_edge_converter_flags=tfl_converter_flags)
+edge_model.export("./edgeface-xs.tflite")
